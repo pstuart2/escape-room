1c72f466ecab1d1d185977f02ce1b111452c5eb2
Unknown <pstuart2@gmail.com>
2017-11-22 19:48:51 -0600

    Now able to delete a game

--- imports/client/ui/GameList.jsx
+++ imports/client/ui/GameList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Game, initialGame, getGameStateString } from '../../api/game.js';
+import { Game, getGameStateString, initialGame } from '../../api/game.js';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Link } from 'react-router-dom';
 import moment from 'moment'
@@ -10,6 +10,22 @@ function formatTimeNumber(n) {
 }
 
 class GameItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onDelete = this.onDelete.bind(this);
+    }
+
+    onDelete(e) {
+        e.preventDefault();
+
+        const { game } = this.props;
+
+        if ( confirm('Are you sure you want to delete this game?') ) {
+            Game.remove({_id: game._id});
+        }
+    }
+
     render() {
         const { game } = this.props;
         const duration = moment.duration(game.time.seconds, 'seconds');
@@ -28,16 +44,18 @@ class GameItem extends Component {
                             <ul>
                                 <li>Players: {game.players.length}</li>
                                 <li>Commands: {game.commandsSent.length}</li>
-                                <li>Time: {formatTimeNumber(duration.hours())}:{formatTimeNumber(duration.minutes())}:{formatTimeNumber(duration.seconds())}</li>
+                                <li>
+                                    Time: {formatTimeNumber(duration.hours())}:{formatTimeNumber(duration.minutes())}:{formatTimeNumber(duration.seconds())}</li>
                                 <li>Times Paused: {game.timesPaused}</li>
-                                <li>Time Paused: {formatTimeNumber(pauseDuration.hours())}:{formatTimeNumber(pauseDuration.minutes())}:{formatTimeNumber(pauseDuration.seconds())}</li>
+                                <li>Time
+                                    Paused: {formatTimeNumber(pauseDuration.hours())}:{formatTimeNumber(pauseDuration.minutes())}:{formatTimeNumber(pauseDuration.seconds())}</li>
                             </ul>
                         </div>
                     </div>
                     <footer className="card-footer">
                         <Link to={`/${game._id}/control`} className="card-footer-item">Control</Link>
                         <Link to={`/${game._id}/secret`} className="card-footer-item">Display</Link>
-                        <a href="#" className="card-footer-item has-text-danger">Delete</a>
+                        <a onClick={this.onDelete} href="#" className="card-footer-item has-text-danger">Delete</a>
                     </footer>
                 </div>
             </div>
