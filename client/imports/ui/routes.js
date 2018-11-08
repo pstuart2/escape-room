df72fb7ab684e7e682c774ea0c476091fa723337
Paul Stuart <pstuart2@gmail.com>
2018-11-08 06:55:03 -0600

    Able to create simple game with name

--- client/imports/ui/routes.js
+++ client/imports/ui/routes.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Route, Router } from 'react-router';
+import {Route, Router} from 'react-router';
 import createBrowserHistory from 'history/createBrowserHistory';
 import {App} from './App'
-import {Dashboard} from "./Dashboard";
 
 const browserHistory = createBrowserHistory();
 
 export const renderRoutes = () => (
     <Router history={browserHistory}>
         <>
-            <Route exact path="/dashboard" component={Dashboard}/>
             <Route exact path="/" component={App}/>
         </>
     </Router>
