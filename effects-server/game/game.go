e7e6ceb594a4eef7e5a7035074a0f4a1a1731e2b
Paul Stuart <pstuart2@gmail.com>
2019-01-07 12:31:20 -0600

    Added game collection to game hooks

--- effects-server/game/game.go
+++ effects-server/game/game.go
@@ -1,51 +1,54 @@
 package game
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/globalsign/mgo"
+	"github.com/sirupsen/logrus"
+)
 
 var log *logrus.Entry
 
-func OnStarting(g *Game) {
+func OnStarting(g *Game, games *mgo.Collection) {
 	log = logrus.WithFields(logrus.Fields{
 		"component": "game",
 		"gameId":    g.ID,
 	})
 }
 
-func OnStartingTick(g *Game) {
+func OnStartingTick(g *Game, games *mgo.Collection) {
 
 }
 
-func OnStart(g *Game) {
+func OnStart(g *Game, games *mgo.Collection) {
 
 }
 
-func OnRunningTick(g *Game) {
+func OnRunningTick(g *Game, games *mgo.Collection) {
 
 }
 
-func OnPause(g *Game) {
+func OnPause(g *Game, games *mgo.Collection) {
 
 }
 
-func OnPauseTick(g *Game) {
+func OnPauseTick(g *Game, games *mgo.Collection) {
 
 }
 
-func OnResume(g *Game) {
+func OnResume(g *Game, games *mgo.Collection) {
 
 }
 
-func OnStop(g *Game) {
+func OnStop(g *Game, games *mgo.Collection) {
 }
 
-func OnMotionChange(g *Game, hasMotion int) {
-
-}
-
-func OnDistanceChange(g *Game, distance float64) {
+func OnMotionChange(g *Game, games *mgo.Collection, hasMotion int) {
 
 }
 
-func OnRfid(g *Game, id int64, text string) {
+func OnDistanceChange(g *Game, games *mgo.Collection, distance float64) {
+
+}
+
+func OnRfid(g *Game, games *mgo.Collection, id int64, text string) {
 
 }
